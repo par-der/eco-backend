ccf323d94bf31c61c3e32769de93495a415bca35
par-der <egorterentev58@gmail.com>
2025-06-26 16:07:47 +0400

    work project in TWA and server

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,8 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/webapp/',
   plugins: [react(), tailwindcss(),],
   server: {
+    host: true,
+    allowedHosts: ['.ngrok-free.app'],
     proxy: {
       '/api': 'http://localhost:8080',
     },
